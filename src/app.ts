362ae60ecf10f541db5907d125571eb04a22335a
Tapan Upadhyay <tapanupa4321@gmail.com>
2025-08-11 16:43:37 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -71,7 +71,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
